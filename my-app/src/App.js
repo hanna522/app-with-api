5d3d3fcf043833f3dbb1fcfa150524daabd2db33
hanna522 <120094054+hanna522@users.noreply.github.com>
2024-04-12 16:22:16 -0700

    fetch weather api

---
+++ my-app/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css'
+import Weather from './app_component/weather.component';
+
+const API_key = "429736441cf3572838aa10530929f7cd"
+
+//api call https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
+    this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm"
+    }
+  }
+
+  calCelsius(temp) {
+    let cell = Math.floor(temp-273.15)
+    return cell;
+  }
+
+  getWeather = async() => {
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+    
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celsius: this.calCelsius(response.main.temp),
+      temp_max: this.calCelsius(response.main.temp_max),
+      temp_min: this.calCelsius(response.main.temp_min),
+      description: response.weather[0].description,
+      icon: this.weatherIcon.Thunderstorm
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Weather city={this.state.city} country={this.state.country} temp_celsius={this.state.celsius} temp_max={this.state.temp_max} temp_min={this.state.temp_min} description={this.state.description} weatherIcon={this.state.icon} />        
+      </div>
+    );
+  }
+}
+
+export default App;
